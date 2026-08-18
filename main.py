9fe8079e72e260d1021b50c50507685a1d2006b5
Paulin NZODOUM <78661160+PaulUno777@users.noreply.github.com>
2026-08-18 04:00:46 +0100

    Enhance function calling in chatbot by integrating call_function utility and improving error handling for tool calls

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import json
 import os
 from dotenv import load_dotenv
 import argparse
 
 from prompts import system_prompt
-from functions.call_function import available_functions
+from functions.call_function import available_functions, call_function
 
 parser = argparse.ArgumentParser(description="Chatbot")
 
@@ -45,8 +44,11 @@ def main():
     message = response.choices[0].message
     if message.tool_calls:
         for tool_call in message.tool_calls:
-            function_args = json.loads(tool_call.function.arguments or "{}")
-            print(f"Calling function: {tool_call.function.name}({function_args})")
+            result_message = call_function(tool_call, verbose=args.verbose)
+            if not result_message.get("content"):
+                raise Exception("no content in tool message")
+            if args.verbose:
+                print(f"-> {result_message['content']}")
     else:
         print(message.content)
 
